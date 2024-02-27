ed855fdb9231ad624acb4e79d570a40a5f7e4720
mohit <connectwithlastdev@gmail.com>
2024-02-27 23:04:05 +0530

    add kickcase

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Project.css'
 import im1 from '../../../public/im1.png'
 import im2 from '../../../public/im2.png'
-import im3 from '../../../public/im3.png'
+import kickcase from '../../../public/kickcase.png'
 
 function Project() {
     return (
@@ -44,34 +44,6 @@ function Project() {
 
                     <div className="project-content">
                         <div className="project-content-1">
-                            <h3>Celestial Notes</h3>
-                            <h4>A notes saving webapp</h4>
-                            <p>Stack- MERN</p>
-                            <p>Frontend Library- React</p>
-                            <p>State Management Library- Recoil</p>
-                            <p>Programming Language- TypeScript</p>
-                        </div>
-
-                        <div className="project-content-2">
-                            <div>
-                                <a href="https://celestialnotes.vercel.app/" className='button' target='_blank'>visit the webapp</a>
-                            </div>
-                            <div>
-                                <a href="https://github.com/last-developer/celestial-notes" className='button' target='_blank'>see the code</a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="project-image">
-                        <img src={im3} alt="image" />
-                    </div>
-                </div>
-
-                {/* project-3 */}
-                <div className='project-container'>
-
-                    <div className="project-content">
-                        <div className="project-content-1">
                             <h3>Ether Watches</h3>
                             <h4>A watch Ecommerce store</h4>
                             <p>Stack- MERN</p>
@@ -92,6 +64,34 @@ function Project() {
 
                     <div className="project-image">
                         <img src={im2} alt="image" />
+                    </div>
+                </div>
+
+                {/* project-3 */}
+                <div className='project-container'>
+
+                    <div className="project-content">
+                        <div className="project-content-1">
+                            <h3>Kickcase Creations</h3>
+                            <h4>A Personalised Drawer selling Startup</h4>
+                            <p>Framework- Next.js</p>
+                            <p>Database- PostgreSQL</p>
+                            <p>State Management Library- Recoil</p>
+                            <p>Programming Language- TypeScript</p>
+                        </div>
+
+                        <div className="project-content-2">
+                            <div>
+                                <a href="https://kickcasecreations.in/" className='button' target='_blank'>visit the webapp</a>
+                            </div>
+                            <div>
+                                <a href="https://github.com/last-developer/kickcase_creations" className='button' target='_blank'>see the code</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="project-image">
+                        <img src={kickcase} alt="image" />
                     </div>
                 </div>
             </div>
