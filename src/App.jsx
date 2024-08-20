36401c5eef1079b7762b804937128b753a7f59b4
mohit <connectwithlastdev@gmail.com>
2024-08-20 07:24:57 +0530

    upload frontend resume

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import Header from './components/header/Header';
 import Homepage from './components/homepage/Homepage';
 import Footer from './components/footer/Footer';
 import Resume from './components/Resume';
+import FrontendResume from './components/FrontendResume';
 
 function App() {
   const pathname = window.location.pathname;
   if (pathname === '/resume') {
     return <Resume />;
+  }
+  if (pathname === '/frontendresume') {
+    return <FrontendResume />;
   }
 
   return (
---
+++ src/components/FrontendResume.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const FrontendResume = () => {
+  return (
+      <section style={{ padding: '0px' }}>
+            <div className="resume-content">
+                <iframe
+                    src="/Mohit_Frontend_Resume.pdf"
+                    title="Resume"
+                    width="100%"
+                    height="600px"
+                />
+            </div>
+        </section>
+  )
+}
+
+export default FrontendResume;
