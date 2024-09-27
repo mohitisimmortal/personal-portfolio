a3670d51ab1ae366d8ebf403c59ff41061b94048
mohit <connectwithlastdev@gmail.com>
2024-09-27 12:10:06 +0530

    add web2fullstack resume

---
+++ portfolio/app/web2fullstack/page.tsx
@@ -0,0 +1,11 @@
+export default function Web2Fullstack() {
+    return (
+        <main>
+            <iframe
+                src="/Mohit_Web2_FullStack.pdf"
+                className="w-full h-[100vh]"
+                title="Full Stack Resume"
+            />
+        </main>
+    );
+}
