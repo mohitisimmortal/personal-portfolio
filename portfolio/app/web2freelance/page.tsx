753a5470578e78b4d1381adf0e70058accafce68
mohit <connectwithlastdev@gmail.com>
2024-10-16 19:59:03 +0530

    update resume

---
+++ portfolio/app/web2freelance/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <main>
+            <iframe
+                src="/Mohit_Fullstack_Resume.pdf"
+                className="w-full h-[100vh]"
+                title="Full Stack Resume"
+            />
+        </main>
+  )
+}
+
+export default page
--- portfolio/app/web2fullstack/page.tsx
+++ portfolio/app/web2fullstack/page.tsx
@@ -2,7 +2,7 @@ export default function Web2Fullstack() {
     return (
         <main>
             <iframe
-                src="/Mohit_Web2_FullStack.pdf"
+                src="/Mohit_Web2_Fullstack.pdf"
                 className="w-full h-[100vh]"
                 title="Full Stack Resume"
             />
